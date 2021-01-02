6fcc6d805d9e08c5fa27149dd0ee338d0eb80329
Mahdybk <mahdi.baccouche@esprit.tn>
2021-01-02 19:29:28 +0100

    Add files via upload

--- Views/index.php
+++ Views/index.php
@@ -1,45 +1,32 @@
-<?php
-// On prolonge la session
-session_start();
-// On teste si la variable de session existe et contient une valeur
-if (empty($_SESSION['e'])) {
-	// Si inexistante ou nulle, on redirige vers le formulaire de login
-	header('Location: signin.php');
-}
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
-<!-- Basic -->
-
+<html lang="en"><!-- Basic -->
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+	 <title>Dar Mima </title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<!-- Mobile Metas -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
 
-	<!-- Site Metas -->
-	<title>Dar Mima </title>
-	<meta name="keywords" content="">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<!-- Site Icons -->
-	<link rel="icon" type="image/png" href="images/logob.jpg">
-
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
 	<!-- Site CSS -->
-	<link rel="stylesheet" href="css/style.css">
-	<!-- Responsive CSS -->
-	<link rel="stylesheet" href="css/responsive.css">
-	<!-- Custom CSS -->
-	<link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/style.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
 
-	<!--[if lt IE 9]>
+    <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
@@ -47,49 +34,38 @@ if (empty($_SESSION['e'])) {
 </head>
 
 <body>
-
-
-
 	<!-- Start header -->
 	<header class="top-navbar">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="index.php">
-					<img src="../Views/images/logob.png" height="80" width="250">
+				<img src="../Views/images/logob.png"  height="80" width="250">
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+				  <span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbars-rs-food">
 					<ul class="navbar-nav ml-auto">
-
-						<li class="nav-item active"><a class="nav-link" href="index.php">Accueil</a></li>
+					<li class="nav-item"><a class="nav-link" href="../Controller/connexion.php">Sign Up</a></li>
+					<li class="nav-item"><a class="nav-link" href="../Controller/signin.php">Sign In</a></li>
+                      <li class="nav-item active"><a class="nav-link" href="index.php">Home</a></li>
 						<li class="nav-item"><a class="nav-link" href="menu.php">Menu</a></li>
 						<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
 								<a class="dropdown-item" href="reservation.php">Reservation</a>
-								<a class="dropdown-item" href="stuff.php">Stuff</a> </div>
+								<a class="dropdown-item" href="stuff.php">Stuff</a>							</div>
 						</li>
-
+						
 						<li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Paramètres</a>
-							<div class="dropdown-menu" aria-labelledby="dropdown-a">
-								<a class="dropdown-item" href="modifiermoncompte.php">Modifier mon compte</a>
-								<a class="dropdown-item" href="../Views/deconnexion.php">Déconnexion</a> </div>
-						</li>
-
-						<!--<li class="nav-item"><a class="nav-link" href="../Views/deconnexion.php">Déconnexion</a></li> -->
-
 					</ul>
 				</div>
 			</div>
 		</nav>
 	</header>
 	<!-- End header -->
-
+	
 	<!-- Start slides -->
 	<div id="slides" class="cover-slides">
 		<ul class="slides-container">
@@ -98,7 +74,10 @@ if (empty($_SESSION['e'])) {
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
-							<h1 class="m-b-20"><strong>Bienvenue Fi <br> Dar Mima <br> <?php echo ($_SESSION['e']); ?> </strong></h1>
+							<h1 class="m-b-20"><strong>Welcome To <br> Dar Mimaa</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="reservation.php">Reservation</a></p>
 						</div>
 					</div>
 				</div>
@@ -108,7 +87,10 @@ if (empty($_SESSION['e'])) {
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
-							<h1 class="m-b-20"><strong>Bienvenue Fi <br> Dar Mima <br> <?php echo ($_SESSION['e']); ?> </strong></h1>
+							<h1 class="m-b-20"><strong>Welcome To <br> Dar Mimaa</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
 						</div>
 					</div>
 				</div>
@@ -118,7 +100,10 @@ if (empty($_SESSION['e'])) {
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
-							<h1 class="m-b-20"><strong>Bienvenue Fi <br> Dar Mima <br> <?php echo ($_SESSION['e']); ?> </strong></h1>
+							<h1 class="m-b-20"><strong>Welcome To <br> Dar Mimaa</strong></h1>
+							<p class="m-b-40">See how your users experience your website in realtime or view  <br> 
+							trends to see any changes in performance over time.</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a></p>
 						</div>
 					</div>
 				</div>
@@ -130,10 +115,8 @@ if (empty($_SESSION['e'])) {
 		</div>
 	</div>
 	<!-- End slides -->
-
+	
 	<!-- Start About -->
-
-	<form action="index.php" method="POST">
 	<div class="about-section-box">
 		<div class="container">
 			<div class="row">
@@ -142,36 +125,18 @@ if (empty($_SESSION['e'])) {
 				</div>
 				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
 					<div class="inner-column">
-						<h1> <?php
-								// Il est bien connecté
-								echo 'Bienvenue ', $_SESSION['e'];
-								echo ('<script> alert("Bienvenue, voici notre page d accueil"); </script>');
-								?>
-						</h1>
-						<h1>Fi Dar mimaa</h1>
-						<p>Choisissez la spécialité de vos plats préférés</p>
-						<div class="input-group">
-							<div>
-								<select class="form-control" id="sp" name="sp" onClick="specialite()" >
-									<option value="select">------------------------------------select--------------------------------------</option>
-									<option value="kaftaji">Kaftaji</option>
-									<option value="Brik">Brik</option>
-									<option value="Couscous">Couscous</option>
-
-								</select>
-							</div>
-						</div>
-						<label id="elementsp" name="erreur" style="color: red;display: none;">Veuillez choisir une spécialité </label>
-
-						<a class="btn btn-lg btn-circle btn-outline-new-white" type="submit" name="submit" value="Voir les Restaurants"onClick="validation()">Voir les Restaurants</a>
+						<h1>Welcome To Dar mimaa</h1>
+						<h4>Little Story</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque auctor suscipit feugiat. Ut at pellentesque ante, sed convallis arcu. Nullam facilisis, eros in eleifend luctus, odio ante sodales augue, eget lacinia lectus erat et sem. </p>
+						<p>Sed semper orci sit amet porta placerat. Etiam quis finibus eros. Sed aliquam metus lorem, a pellentesque tellus pretium a. Nulla placerat elit in justo vestibulum, et maximus sem pulvinar.</p>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="#">Reservation</a>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</form>
 	<!-- End About -->
-
+	
 	<!-- Start QT -->
 	<div class="qt-box qt-background">
 		<div class="container">
@@ -186,7 +151,7 @@ if (empty($_SESSION['e'])) {
 		</div>
 	</div>
 	<!-- End QT -->
-
+	
 	<!-- Start Menu -->
 	<div class="menu-box">
 		<div class="container">
@@ -210,7 +175,7 @@ if (empty($_SESSION['e'])) {
 					</div>
 				</div>
 			</div>
-
+				
 			<div class="row special-list">
 				<div class="col-lg-4 col-md-6 special-grid drinks">
 					<div class="gallery-single fix">
@@ -218,11 +183,11 @@ if (empty($_SESSION['e'])) {
 						<div class="why-text">
 							<h4>Special Drinks 1</h4>
 							<p>citronade tunisienp>
-								<h5>5d </h5>
+							<h5>5d </h5>
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid drinks">
 					<div class="gallery-single fix">
 						<img src="images/img-02.jpg" class="img-fluid" alt="Image">
@@ -233,7 +198,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid drinks">
 					<div class="gallery-single fix">
 						<img src="images/img-03.jpg" class="img-fluid" alt="Image">
@@ -244,7 +209,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid lunch">
 					<div class="gallery-single fix">
 						<img src="images/img-04.jpg" class="img-fluid" alt="Image">
@@ -255,7 +220,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid lunch">
 					<div class="gallery-single fix">
 						<img src="images/img-05.jpg" class="img-fluid" alt="Image">
@@ -266,7 +231,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid lunch">
 					<div class="gallery-single fix">
 						<img src="images/img-06.jpg" class="img-fluid" alt="Image">
@@ -277,7 +242,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid dinner">
 					<div class="gallery-single fix">
 						<img src="images/img-07.jpg" class="img-fluid" alt="Image">
@@ -288,7 +253,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid dinner">
 					<div class="gallery-single fix">
 						<img src="images/img-08.jpg" class="img-fluid" alt="Image">
@@ -299,7 +264,7 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 				<div class="col-lg-4 col-md-6 special-grid dinner">
 					<div class="gallery-single fix">
 						<img src="images/img-09.jpg" class="img-fluid" alt="Image">
@@ -310,12 +275,12 @@ if (empty($_SESSION['e'])) {
 						</div>
 					</div>
 				</div>
-
+				
 			</div>
 		</div>
 	</div>
 	<!-- End Menu -->
-
+	
 	<!-- Start Gallery -->
 	<div class="gallery-box">
 		<div class="container-fluid">
@@ -353,7 +318,7 @@ if (empty($_SESSION['e'])) {
 						<a class="lightbox" href="images/gallery-img-05.jpg">
 							<img class="img-fluid" src="images/gallery-img-05.jpg" alt="Gallery Images">
 						</a>
-					</div>
+					</div> 
 					<div class="col-sm-6 col-md-4 col-lg-4">
 						<a class="lightbox" href="images/gallery-img-06.jpg">
 							<img class="img-fluid" src="images/gallery-img-06.jpg" alt="Gallery Images">
@@ -364,9 +329,9 @@ if (empty($_SESSION['e'])) {
 		</div>
 	</div>
 	<!-- End Gallery -->
-
-
-
+	
+	
+	
 	<!-- Start Contact info -->
 	<div class="contact-imfo-box">
 		<div class="container">
@@ -389,13 +354,13 @@ if (empty($_SESSION['e'])) {
 						</p>
 					</div>
 				</div>
-
-
+				
+				
 			</div>
 		</div>
 	</div>
 	<!-- End Contact info -->
-
+	
 	<!-- Start Footer -->
 	<footer class="footer-area bg-f">
 		<div class="container">
@@ -435,56 +400,34 @@ if (empty($_SESSION['e'])) {
 				</div>
 			</div>
 		</div>
-
+		
 		<div class="copyright">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
-						<p class="company-name">All Rights Reserved. &copy; 2018 <a href="#">Dar Mimaa</a> Design By :
-							<a href="https://html.design/">html design</a></p>
+						<p class="company-name">All Rights Reserved. &copy; 2018 <a href="#">Dar Mimaa</a> Design By : 
+					<a href="https://html.design/">html design</a></p>
 					</div>
 				</div>
 			</div>
 		</div>
-
+		
 	</footer>
 	<!-- End Footer -->
-
+	
 	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
 
 	<!-- ALL JS FILES -->
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/popper.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<!-- ALL PLUGINS -->
+    <!-- ALL PLUGINS -->
 	<script src="js/jquery.superslides.min.js"></script>
 	<script src="js/images-loded.min.js"></script>
 	<script src="js/isotope.min.js"></script>
 	<script src="js/baguetteBox.min.js"></script>
 	<script src="js/form-validator.min.js"></script>
-	<script src="js/contact-form-script.js"></script>
-	<script src="js/custom.js"></script>
-
-	<script>
-		function specialite() {
-			var sp = document.getElementById("sp").value;
-			var element = document.getElementById("elementsp");
-			if (sp === "select") {
-				element.style.display = "block";
-			} else {
-				element.style.display = "none";
-			}
-		}
-		function validation() {
-			var element = document.getElementById("elementsp");
-			var sp = document.getElementById("sp").value;
-
-			if ((element.style.display === "none")&&(sp!=="select")) {
-				window.location.href="reservation.php" ;
-			} 
-		}
-	</script>
-
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
 </body>
-
 </html>
