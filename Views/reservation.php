26aab1bd2b31f0f85b346fe023335c1257e3af4b
Mahdybk <mahdi.baccouche@esprit.tn>
2021-01-02 19:27:52 +0100

    Update reservation.php

--- Views/reservation.php
+++ Views/reservation.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
-<html lang="en">
-<!-- Basic -->
-
+<html lang="en"><!-- Basic -->
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+	 <title>Dar Mima </title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<!-- Mobile Metas -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
 
-	<!-- Site Metas -->
-	<title>Dar Mima </title>
-	<meta name="keywords" content="">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<!-- Site Icons -->
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
 	<!-- Site CSS -->
-	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">    
 	<!-- Pickadate CSS -->
-	<link rel="stylesheet" href="css/classic.css">
-	<link rel="stylesheet" href="css/classic.date.css">
-	<link rel="stylesheet" href="css/classic.time.css">
-	<!-- Responsive CSS -->
-	<link rel="stylesheet" href="css/responsive.css">
-	<!-- Custom CSS -->
-	<link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/classic.css">    
+	<link rel="stylesheet" href="css/classic.date.css">    
+	<link rel="stylesheet" href="css/classic.time.css">    
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
 
-	<!--[if lt IE 9]>
+    <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
@@ -45,15 +43,16 @@
 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="index.html">
-					<img src="../Views/images/logob.png" height="80" width="250">
+				<img src="../Views/images/logob.png"  height="80" width="250">
 				</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+				  <span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbars-rs-food">
 					<ul class="navbar-nav ml-auto">
-
-						<li class="nav-item"><a class="nav-link" href="index.php">Accueil</a></li>
+					<li class="nav-item"><a class="nav-link" href="../Controller/connexion.php">Sign Up</a></li>
+					<li class="nav-item"><a class="nav-link" href="../Controller/signin.php">Sign In</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
 						<li class="nav-item"><a class="nav-link" href="menu.php">Menu</a></li>
 						<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
 						<li class="nav-item active dropdown">
@@ -61,19 +60,17 @@
 							<div class="dropdown-menu" aria-labelledby="dropdown-a">
 								<a class="dropdown-item" href="reservation.php">Reservation</a>
 								<a class="dropdown-item" href="stuff.php">livraison</a>
-
+								
 							</div>
 						</li>
 						<li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-						<li class="nav-item"><a class="nav-link" href="../Views/deconnexion.php">Déconnexion</a></li>
-
 					</ul>
 				</div>
 			</div>
 		</nav>
 	</header>
 	<!-- End header -->
-
+	
 	<!-- Start All Pages -->
 	<div class="all-page-title page-breadcrumb">
 		<div class="container text-center">
@@ -85,7 +82,7 @@
 		</div>
 	</div>
 	<!-- End All Pages -->
-
+	
 	<!-- Start Reservation -->
 	<div class="reservation-box">
 		<div class="container">
@@ -93,84 +90,45 @@
 				<div class="col-lg-12">
 					<div class="heading-title text-center">
 						<h2>Reservation</h2>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+						
 					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-lg-12 col-sm-12 col-xs-12">
 					<div class="contact-block">
-						<form id="contactForm">
-							<div class="row">
-								<div class="col-md-6">
-									<h3>Book a table</h3>
-									<div class="col-md-12">
-										<div class="form-group">
-											<input id="input_date" class="datepicker picker__input form-control" name="date" type="text" value="" equired data-error="Please enter Date">
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-									<div class="col-md-12">
-										<div class="form-group">
-											<input id="input_time" class="time form-control picker__input" required data-error="Please enter time">
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-									<div class="col-md-12">
-										<div class="form-group">
-											<select class="custom-select d-block form-control" id="person" required data-error="Please select Person">
-												<option disabled selected>Select Person*</option>
-												<option value="1">1</option>
-												<option value="2">2</option>
-												<option value="3">3</option>
-												<option value="4">4</option>
-												<option value="5">5</option>
-												<option value="6">6</option>
-												<option value="7">7</option>
-											</select>
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-6">
-									<h3>Contact Details</h3>
-									<div class="col-md-12">
-										<div class="form-group">
-											<input type="text" class="form-control" id="name" name="name" placeholder="Your Name" required data-error="Please enter your name">
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-									<div class="col-md-12">
-										<div class="form-group">
-											<input type="text" placeholder="Your Email" id="email" class="form-control" name="email" required data-error="Please enter your email">
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-									<div class="col-md-12">
-										<div class="form-group">
-											<input type="text" placeholder="Your Numbar" id="phone" class="form-control" name="phone" required data-error="Please enter your Numbar">
-											<div class="help-block with-errors"></div>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-12">
-									<div class="submit-button text-center">
-										<button class="btn btn-common" id="submit" type="submit">Book Table</button>
-										<div id="msgSubmit" class="h3 text-center hidden"></div>
-										<div class="clearfix"></div>
-									</div>
-								</div>
-							</div>
-						</form>
+					<form class="text-left clearfix" method="post" action="AjouterUser.php">
+            <div class="form-group">
+              <input type="text" class="form-control" name="nom" placeholder=" Name">
+            </div>
+            <div class="form-group">
+              <input type="date" class="form-control" name="date" placeholder="date">
+            </div>
+            <div class="form-group">
+              <input type="text" class="form-control" name="time" placeholder="donner le temps">
+            </div>
+            <div class="form-group">
+              <input type="email" class="form-control" name="email" placeholder="Email">
+            </div>
+			  <div class="form-group">
+              <input type="text" class="form-control" name="tel" placeholder="numero">
+            </div>
+			  <div class="form-group">
+              <input type="number" class="form-control" name="pers" placeholder="nombre du personne">
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-main text-center">Confirmer</button>
+            </div>
+          </form>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 	<!-- End Reservation -->
+	
 
-
-
+	
 	<!-- Start Contact info -->
 	<div class="contact-imfo-box">
 		<div class="container">
@@ -200,7 +158,7 @@
 		</div>
 	</div>
 	<!-- End Contact info -->
-
+	
 	<!-- Start Footer -->
 	<footer class="footer-area bg-f">
 		<div class="container">
@@ -240,28 +198,28 @@
 				</div>
 			</div>
 		</div>
-
+		
 		<div class="copyright">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
-						<p class="company-name">All Rights Reserved. &copy; 2018 <a href="#">Dar Mimaa</a> Design By :
-							<a href="https://html.design/">html design</a></p>
+						<p class="company-name">All Rights Reserved. &copy; 2018 <a href="#">Dar Mimaa</a> Design By : 
+					<a href="https://html.design/">html design</a></p>
 					</div>
 				</div>
 			</div>
 		</div>
-
+		
 	</footer>
 	<!-- End Footer -->
-
+	
 	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
 
 	<!-- ALL JS FILES -->
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/popper.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<!-- ALL PLUGINS -->
+    <!-- ALL PLUGINS -->
 	<script src="js/jquery.superslides.min.js"></script>
 	<script src="js/images-loded.min.js"></script>
 	<script src="js/isotope.min.js"></script>
@@ -271,8 +229,7 @@
 	<script src="js/picker.time.js"></script>
 	<script src="js/legacy.js"></script>
 	<script src="js/form-validator.min.js"></script>
-	<script src="js/contact-form-script.js"></script>
-	<script src="js/custom.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
 </body>
-
 </html>
